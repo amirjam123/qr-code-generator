9682ed19a4d4715fa5327a86638453cab6fb51aa
rango <amirjam123amir@gmail.com>
2026-07-04 08:02:41 -0400

    feat: show commands list in /start message

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,6 +2,9 @@ import { Context } from "telegraf";
 
 export async function handleStart(ctx: Context): Promise<void> {
   await ctx.reply(
-    "سلام به ربات خوش آمدید. این ربات یک سازنده کیو ار کد می باشد. توسط رنگو ساخته شده."
+    "سلام به ربات خوش آمدید. این ربات یک سازنده کیو ار کد می باشد. توسط رنگو ساخته شده.\n\n" +
+    "دستورات ربات:\n\n" +
+    "/generate - ساخت کیو ار کد\n" +
+    "/cancel - لغو عملیات"
   );
 }
